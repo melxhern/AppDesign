c935b2d4928458606b3b738fb899336d52c9d6b8
Mélanie <melxhern@gmail.com>
2024-01-09 11:21:40 +0100

    debut

---
+++ app/src/androidTest/java/com/melanie/myapplication/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.melanie.myapplication
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ * permet de tester l'interface (verifier si il y a pas de crash, si les boutons ont les bonnes fonctionnalités)
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.melanie.myapplication", appContext.packageName)
+    }
+}
